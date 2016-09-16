861fa76f8dec8633d5ad34e4dab3b28ac8d6f4e7
Jesse Hanley <jesse@jessehanley.com>
2016-09-16 16:25:41 +1000

    added function for footer menu

--- functions.php
+++ functions.php
@@ -4,6 +4,7 @@ add_theme_support( 'post-thumbnails' );
 
 function register_my_menu() {
   register_nav_menu('header-menu',__( 'Header Menu' ));
+  register_nav_menu('footer-menu',__( 'Footer Menu' ));
 }
 add_action( 'init', 'register_my_menu' );
 
